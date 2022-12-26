7190315495a801724b30a9f8a6594b72cbd89d69
lgruchala7 <l.gruchala7@gmail.com>
2022-12-26 16:12:21 +0000

    App icon, launch screen, startup animation added

---
+++ app/src/main/java/com/example/booxchange/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.booxchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+
+    }
+}
