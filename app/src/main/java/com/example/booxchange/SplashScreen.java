38c3ee10e9af3f156d3cedef49a13ba46a4167df
lgruchala7 <l.gruchala7@gmail.com>
2022-12-27 22:39:27 +0000

    Bottom navigation bar and dashboard

--- app/src/main/java/com/example/booxchange/SplashScreen.java
+++ app/src/main/java/com/example/booxchange/SplashScreen.java
@@ -23,7 +23,7 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 5000);
+        }, 4500);
 
     }
 }
