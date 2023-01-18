a138c963c138af40264a77316f5372dc07e2b21b
lgruchala7 <l.gruchala7@gmail.com>
2023-01-18 20:54:57 +0000

    Add Google directions

--- app/src/main/java/com/example/booxchange/activities/MapsActivity.java
+++ app/src/main/java/com/example/booxchange/activities/MapsActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
 
@@ -21,8 +22,9 @@ import android.graphics.drawable.VectorDrawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationRequest;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -31,11 +33,16 @@ import com.example.booxchange.R;
 import com.example.booxchange.adapters.AdInfoWindowAdapter;
 import com.example.booxchange.databinding.ActivityMapsBinding;
 import com.example.booxchange.models.Ad;
+import com.example.booxchange.models.PolylineData;
 import com.example.booxchange.models.User;
 import com.example.booxchange.utilities.Constants;
 import com.example.booxchange.utilities.PreferenceManager;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -45,6 +52,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentChange;
@@ -52,6 +61,14 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PendingResult;
+import com.google.maps.internal.PolylineEncoding;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+
+import org.checkerframework.checker.units.qual.A;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -60,20 +77,25 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMapClickListener {
+public class MapsActivity extends FragmentActivity implements
+        OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMapClickListener, GoogleMap.OnPolylineClickListener {
 
     private static final int REQUEST_CODE = 101;
+    private static final String TAG = MapsActivity.class.getSimpleName();
 
-    private GoogleMap mMap;
+    private GoogleMap googleMap;
     private ActivityMapsBinding binding;
     private FusedLocationProviderClient fusedLocationClient;
     private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
     private Location currentLocation;
     private boolean isPermissionGranted;
     private List<Ad> ads;
     private PreferenceManager preferenceManager;
     private Marker currentMarker;
-    FirebaseFirestore database;
+    private FirebaseFirestore database;
+    private GeoApiContext geoApiContext;
+    private ArrayList<PolylineData> polylinesData = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +106,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         getCurrentLocation();
+//        setLocationRequest();
 
         if (isPermissionGranted) {
             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -135,7 +158,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 finish();
             }
         });
-        binding.fabStartChat.setOnClickListener(v -> {
+        binding.fabStartChat.setOnClickListener( v -> {
             if (currentMarker != null) {
                 int adIndex = Integer.parseInt(currentMarker.getSnippet());
                 Ad ad = ads.get(adIndex);
@@ -147,9 +170,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 startActivity(intent);
             }
         });
-        binding.fabAddToFav.setOnClickListener(v -> {
+        binding.fabAddToFav.setOnClickListener( v -> {
             int adIndex = Integer.parseInt(currentMarker.getSnippet());
             getFavoriteAdId(adIndex);
+        });
+        binding.fabGetDirections.setOnClickListener( v -> {
+            binding.fabGetDirections.setVisibility(View.GONE);
+            binding.fabCloseDirections.setVisibility(View.VISIBLE);
+            calculateDirections(currentMarker);
+            centerOnMyLocation();
+        });
+        binding.fabFocusOnMyLocation.setOnClickListener( v -> centerOnMyLocation() );
+        binding.fabCloseDirections.setOnClickListener ( v -> {
+            binding.fabGetDirections.setVisibility(View.VISIBLE);
+            binding.fabCloseDirections.setVisibility(View.GONE);
         });
     }
 
@@ -270,7 +304,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                     Bitmap markerBitmap = getBitmapFromDrawable(getApplicationContext(), R.drawable.ic_book);
 
-                    Marker marker = mMap.addMarker(
+                    Marker marker = googleMap.addMarker(
                             new MarkerOptions()
                                     .position(latLng)
                                     .snippet(Integer.toString(i))
@@ -317,13 +351,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 });
     }
 
+    @SuppressLint("MissingPermission")
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        
-        LatLng currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        mMap.addMarker(new MarkerOptions().position(currentLatLng).title("My current location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 14.0f));
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+
         try {
             boolean success = googleMap.setMapStyle(
                     MapStyleOptions.loadRawResourceStyle(
@@ -335,16 +367,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Log.e(this.getLocalClassName(), "Can't find style. Error: ", e);
         }
 
+        if (geoApiContext == null) {
+            geoApiContext = new GeoApiContext.Builder()
+                    .apiKey(getString(R.string.google_api_key))
+                    .build();
+        }
 
-        mMap.setInfoWindowAdapter(new AdInfoWindowAdapter(MapsActivity.this, ads));
+        centerOnMyLocation();
+        googleMap.setOnPolylineClickListener(this);
+        googleMap.setMyLocationEnabled(true);
+        googleMap.setInfoWindowAdapter(new AdInfoWindowAdapter(MapsActivity.this, ads));
         setMapListeners();
         listenAds();
     }
 
     private void setMapListeners() {
-        mMap.setOnMarkerClickListener(this);
-        mMap.setOnMapClickListener(this);
-        mMap.setOnInfoWindowClickListener( marker -> {
+        googleMap.setOnMarkerClickListener(this);
+        googleMap.setOnMapClickListener(this);
+        googleMap.setOnInfoWindowClickListener( marker -> {
             int adIndex = Integer.parseInt(marker.getSnippet());
             Ad ad = ads.get(adIndex);
 
@@ -373,6 +413,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    @SuppressLint("MissingPermission")
     private void getCurrentLocation() {
         if (ActivityCompat.checkSelfPermission(
                 MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
@@ -381,19 +422,125 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
 
-        @SuppressLint("MissingPermission") Task<Location> task = fusedLocationClient.getLastLocation();
-
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener( location -> {
                 if (location != null) {
                     currentLocation = location;
                     SupportMapFragment supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
                     assert supportMapFragment != null;
                     supportMapFragment.getMapAsync(MapsActivity.this);
+
+                    setLocationRequest();
+                    fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+                }
+            });
+    }
+
+    private void calculateDirections(Marker marker){
+        Log.d(TAG, "calculateDirections: calculating directions.");
+
+        com.google.maps.model.LatLng destination = new com.google.maps.model.LatLng(
+                marker.getPosition().latitude,
+                marker.getPosition().longitude
+        );
+        DirectionsApiRequest directions = new DirectionsApiRequest(geoApiContext);
+
+        directions.alternatives(true);
+        directions.origin(new com.google.maps.model.LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()));
+
+        Log.d(TAG, "calculateDirections: destination: " + destination.toString());
+        directions.destination(destination).setCallback(new PendingResult.Callback<DirectionsResult>() {
+            @Override
+            public void onResult(DirectionsResult result) {
+                Log.d(TAG, "calculateDirections: routes: " + result.routes[0].toString());
+                Log.d(TAG, "calculateDirections: duration: " + result.routes[0].legs[0].duration);
+                Log.d(TAG, "calculateDirections: distance: " + result.routes[0].legs[0].distance);
+                Log.d(TAG, "calculateDirections: geocodedWayPoints: " + result.geocodedWaypoints[0].toString());
+                addPolylinesToMap(result);
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+                Log.e(TAG, "calculateDirections: Failed to get directions: " + e.getMessage() );
+            }
+        });
+    }
+
+    private void addPolylinesToMap(final DirectionsResult result){
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "run: result routes: " + result.routes.length);
+
+                if (polylinesData.size() > 0) {
+                    for (PolylineData polylineData : polylinesData) {
+                        polylineData.getPolyline().remove();
+                    }
+                    polylinesData.clear();
+                    polylinesData = new ArrayList<>();
+                }
+
+                for (DirectionsRoute route: result.routes) {
+                    Log.d(TAG, "run: leg: " + route.legs[0].toString());
+                    List<com.google.maps.model.LatLng> decodedPath = PolylineEncoding.decode(route.overviewPolyline.getEncodedPath());
+
+                    List<LatLng> newDecodedPath = new ArrayList<>();
+
+                    // This loops through all the LatLng coordinates of ONE polyline.
+                    for (com.google.maps.model.LatLng latLng: decodedPath) {
+
+//                        Log.d(TAG, "run: latlng: " + latLng.toString());
+
+                        newDecodedPath.add(new LatLng(
+                                latLng.lat,
+                                latLng.lng
+                        ));
+                    }
+                    Polyline polyline = googleMap.addPolyline(new PolylineOptions().addAll(newDecodedPath));
+                    polyline.setColor(ContextCompat.getColor(MapsActivity.this, R.color.secondary_text));
+                    polyline.setClickable(true);
+                    polylinesData.add(new PolylineData(polyline, route.legs[0]));
                 }
             }
         });
+    }
+
+    private void setLocationRequest() {
+        locationRequest = new com.google.android.gms.location.LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000)
+                .setMinUpdateIntervalMillis(5*1000)
+                .setWaitForAccurateLocation(true)
+                .build();
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    if (location != null) {
+                        currentLocation.setLatitude(location.getLatitude());
+                        currentLocation.setLongitude(location.getLongitude());
+//                        LatLng currentLatLng = new LatLng(
+//                                currentLocation.getLatitude(),
+//                                currentLocation.getLongitude());
+
+//                        List<LatLng> userTrailPoints= userTrail.getPoints();
+//                        userTrailPoints.add(currentLatLng);
+//                        userTrail.setPoints(userTrailPoints);
+
+//                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
+                    }
+                }
+            }
+        };
+    }
+
+    private void centerOnMyLocation() {
+        LatLng currentLatLng = new LatLng(
+                currentLocation.getLatitude(),
+                currentLocation.getLongitude());
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 14.0f));
     }
 
     @Override
@@ -403,6 +550,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             case REQUEST_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getCurrentLocation();
+//                    setLocationRequest();
                 }
                 break;
         }
@@ -412,6 +560,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public boolean onMarkerClick(@NonNull Marker marker) {
         binding.fabAddToFav.setVisibility(View.VISIBLE);
         binding.fabStartChat.setVisibility(View.VISIBLE);
+        binding.fabGetDirections.setVisibility(View.VISIBLE);
         currentMarker = marker;
         return false;
     }
@@ -420,6 +569,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapClick(@NonNull LatLng latLng) {
         binding.fabAddToFav.setVisibility(View.GONE);
         binding.fabStartChat.setVisibility(View.GONE);
+        binding.fabGetDirections.setVisibility(View.GONE);
         currentMarker = null;
+    }
+
+    @Override
+    public void onPolylineClick(Polyline polyline) {
+
+        for(PolylineData polylineData: polylinesData){
+            Log.d(TAG, "onPolylineClick: toString: " + polylineData.toString());
+            if (polyline.getId().equals(polylineData.getPolyline().getId())) {
+                polylineData.getPolyline().setColor(ContextCompat.getColor(getApplicationContext(), R.color.polyline_active));
+                polylineData.getPolyline().setZIndex(1);
+            }
+            else {
+                polylineData.getPolyline().setColor(ContextCompat.getColor(getApplicationContext(), R.color.secondary_text));
+                polylineData.getPolyline().setZIndex(0);
+            }
+        }
     }
 }
